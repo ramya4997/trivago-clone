cf8b3dc9ff9f101a87868731217e9afece26145c
Ramya Krishna <rkramyakrishna561@gmail.com>
2018-06-25 22:27:14 +0530

    added the list of cities

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  var topCitiesCarousel = $("#top-cities");
+  var toppDestCarousel = $("#top-destinations");
+  topCitiesCarousel.owlCarousel({
+    margin: 10,
+  });
+
+  toppDestCarousel.owlCarousel({
+    margin: 10,
+  });
+});
