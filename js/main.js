37c600150a445383f5dfb24a06f32fb382ae40b5
Ramya Krishna <rkramyakrishna561@gmail.com>
2018-06-26 19:43:34 +0530

    completed the site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   var topCitiesCarousel = $("#top-cities");
   var toppDestCarousel = $("#top-destinations");
   topCitiesCarousel.owlCarousel({
-    margin: 10,
+    margin: 10
   });
 
   toppDestCarousel.owlCarousel({
-    margin: 10,
+    margin: 10
   });
 });
